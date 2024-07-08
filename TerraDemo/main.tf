af2755e1996c0c8b9918d110bc2599e9f0d3b910
StarLord598 <41198155+StarLord598@users.noreply.github.com>
2024-07-08 22:28:20 +0000

    saving final demo save

--- TerraDemo/main.tf
+++ TerraDemo/main.tf
@@ -11,3 +11,27 @@ provider "google" {
   project = "triple-shadow-428819-i0"
   region  = "us-west2-b"
 }
+
+resource "google_storage_bucket" "demo-bucket" {
+  name          = "triple-shadow-428819-i0-terra-bucket"
+  location      = "US"
+  force_destroy = true
+
+  lifecycle_rule {
+    condition {
+      age = 3
+    }
+    action {
+      type = "Delete"
+    }
+  }
+
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
+}
